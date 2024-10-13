5416c20052399491da53bc25958df830d08b7a8b
Michał Gołębiowski <m.golebiowski.fm@gmail.com>
2024-10-13 16:55:37 +0200

    after CR

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,85 @@
 import { useState } from "react";
 import QUESTIONS from "./quizQuestions";
 import QuestionForm from "./components/QuestionForm";
-import ResultForm from "./components/ResultForm";
-import Button from "./components/Button";
+import Result from "./components/Result";
 import QuizTime from "./components/QuizTime";
+
 import "./App.css";
 
 const userAnswers = [];
 
 function App() {
-  const [question, setQuestion] = useState(-1);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(-1);
   const [score, setScore] = useState(0);
-  const [timeLong, setTimeLong] = useState(30);
+  const [amountQuestion, setAmountQuestion] = useState(10);
 
   const nextQuestion = (answer) => {
-    if (question !== -1 && answer === findCorrectAnswerIndex(QUESTIONS[question])) {
+    if (
+      currentQuestionIndex !== -1 &&
+      answer === findCorrectAnswerIndex(QUESTIONS[currentQuestionIndex])
+    ) {
       setScore(score + 1);
     }
-    if (question !== -1) userAnswers.push(answer);
-    setQuestion(question + 1);
+    if (currentQuestionIndex !== -1) userAnswers.push(answer);
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
   };
   const resetQuiz = () => {
     userAnswers.length = 0;
-    setQuestion(-1);
+    setCurrentQuestionIndex(-1);
     setScore(0);
+    setAmountQuestion(10);
   };
   const findCorrectAnswerIndex = (question) => {
     return question.answers.findIndex((answer) => answer.isCorrect);
   };
   const selectTimeLong = (e) => {
-    setTimeLong(parseInt(e.target.value));
+    setAmountQuestion(parseInt(e.target.value));
   };
+
+  const renderStartScreen = () => (
+    <>
+      <h2>Sprawdź swoją wiedzę o programowaniu</h2>
+      <QuizTime onChange={selectTimeLong} />
+      <button onClick={() => nextQuestion(-1)}>Start QUIZ!!!</button>
+    </>
+  );
+
+  const renderQuestionScreen = () => (
+    <>
+      <h3>Pytanie nr {currentQuestionIndex + 1}</h3>
+      <QuestionForm
+        question={QUESTIONS[currentQuestionIndex]}
+        nextQuestion={nextQuestion}
+      />
+      <button onClick={resetQuiz}>Reset QUIZ</button>
+    </>
+  );
+
+  const renderResultScreen = () => (
+    <>
+      <Result
+        arrQuestion={QUESTIONS}
+        arrAnswers={userAnswers}
+        amountQuestion={amountQuestion}
+      />
+      <button onClick={resetQuiz}>Reset QUIZ</button>
+    </>
+  );
+
+  const renderCurrentView = () => {
+    if (currentQuestionIndex === -1) {
+      return renderStartScreen();
+    } else if (currentQuestionIndex !== amountQuestion) {
+      return renderQuestionScreen();
+    } else {
+      return renderResultScreen();
+    }
+  };
+
   return (
     <>
       <h1>QUIZ JAVASCRIPT</h1>
-      {question === -1 ? (
-        <>
-          <h2>Sprawdź swoją wiedzę o programowaniu</h2>
-          <QuizTime onChange={selectTimeLong} />
-          <Button onClick={() => nextQuestion(-1)}>Start QUIZ!!!</Button>
-        </>
-      ) : question !== timeLong ? (
-        <>
-          <h3>Pytanie nr {question + 1}</h3>
-          <QuestionForm question={QUESTIONS[question]} nextQuestion={nextQuestion} />
-          <Button onClick={resetQuiz}>Reset QUIZ</Button>
-        </>
-      ) : (
-        <>
-          <ResultForm
-            arrQuestion={QUESTIONS}
-            arrAnswers={userAnswers}
-            timeLong={timeLong}
-          />
-          <Button onClick={resetQuiz}>Reset QUIZ</Button>
-        </>
-      )}
+      {renderCurrentView()}
     </>
   );
 }
